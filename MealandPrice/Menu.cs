d6aa5ec3049ead594b3ae3ce164f668945e5fa8a
HUAWEI <HUAWEI@LAPTOP-H6PN7ITJ>
2024-04-05 17:40:44 +0800

    Add project files.

---
+++ MealandPrice/Menu.cs
@@ -0,0 +1,30 @@
+﻿namespace MealandPrice
+{
+    public class Menu
+    {
+        public static void DisplayFood()
+        {
+            Console.WriteLine("Chicken - 75 Pesos");
+            Console.WriteLine("Spaghetti - 65 Pesos");
+            Console.WriteLine("Palabok - 65 Pesos");
+            Console.WriteLine("Steak - 120 Pesos");
+            Console.WriteLine("Carbonara - 70 Pesos");
+            Console.WriteLine("Fries(Barbeque, Cheese) - 45 Pesos");
+        }
+        public static void DisplayDrinks()
+        {
+            Console.WriteLine("Iced Tea - 20 Pesos");
+            Console.WriteLine("Coke - 20 Pesos");
+            Console.WriteLine("Pineapple - 20 Pesos");
+            Console.WriteLine("Four Season - 25 Pesos");
+            Console.WriteLine("Coke Float - 35 Pesos");
+        }
+        public static void DisplayDesert()
+        {
+            Console.WriteLine("Vanilla Ice Cream(Small - 30 Pesos Medium - 40 Pesos, Large - 50 Pesos)");
+            Console.WriteLine("Sundae - 65 Pesos");
+            Console.WriteLine("Choco Ice Cream Overload - 70 Pesos");
+            Console.WriteLine("Special Halo-Halo - 99 Pesos");
+        }
+    }
+}
---
+++ RestoMenu/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using MealandPrice;
+
+namespace RestoMenu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cyruz's Restaurant");
+            Console.WriteLine("In our Menu, we have: ");
+            Console.WriteLine("1. Food");
+            Console.WriteLine("2. Drinks");
+            Console.WriteLine("3. Desert");
+            Console.WriteLine("Pick a number to see the meal and price: ");
+            string response = Console.ReadLine();
+
+            switch (response)
+            {
+                case "1":
+                    Menu.DisplayFood();
+                    break;
+
+                case "2":
+                    Menu.DisplayDrinks();
+                    break;
+
+                case "3":
+                    Menu.DisplayDesert();
+                    break;
+
+                default:
+                    {
+                        Console.WriteLine("Invalid option");
+                        break;
+                    }
+            }
+
+        }
+    }
+}
